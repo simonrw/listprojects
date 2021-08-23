0ae4824969cea65d7e73269a54eb74e0b7361e89
Simon Walker <s.r.walker101@googlemail.com>
2021-08-23 15:56:20 +0100

    Adds toml

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,6 @@ use structopt::StructOpt;
 #[derive(StructOpt)]
 struct Opts {}
 
-fn worker(tx: crossbeam_channel::Sender<Arc<dyn SkimItem + 'static>>) {
-    std::thread::sleep(std::time::Duration::from_secs(1));
-    tx.send(Arc::new("test".to_string())).unwrap();
-}
-
 fn main() {
     let _args = Opts::from_args();
     let options = SkimOptionsBuilder::default()
@@ -17,19 +12,4 @@ fn main() {
         .multi(false)
         .build()
         .unwrap();
-
-    let (tx, rx) = crossbeam_channel::bounded(100);
-
-    let thread_tx = tx.clone();
-    let handle = std::thread::spawn(move || worker(thread_tx));
-
-    let selected_items = Skim::run_with(&options, Some(rx))
-        .map(|out| out.selected_items)
-        .unwrap_or_else(|| Vec::new());
-
-    for item in selected_items {
-        println!("{}", item.output());
-    }
-
-    handle.join().unwrap();
 }
